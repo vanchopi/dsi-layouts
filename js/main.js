dd53c436226dae2b3eef70b2dbc894d77d483e7e
Vanchopi <ivan.ugnenko@webregul.ru>
2017-05-21 00:23:52 +0300

    dsi layouts

---
+++ js/main.js
@@ -0,0 +1,23 @@
+$(document).ready(function(){
+	console.log('on load');
+
+	var tm_height = $('.menu-top').height() + 15;
+	console.log('tm_height',tm_height);
+	var z_mrg = 0;
+
+	$(window).scroll(function(){
+		var top = $(this).scrollTop();
+		var elem = $('.top-line-wrp');
+		var sb = $('#sidebar .toggle');
+		if(top + z_mrg < tm_height){
+			elem.removeClass('fxd');
+			sb.removeClass('fxd_sb')
+			elem.css('top',(tm_height - top));
+		}else{
+			elem.css('top',z_mrg);
+			sb.addClass('fxd_sb');
+			elem.addClass('fxd');
+		}
+	})
+})
+
